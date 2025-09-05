23df8a07840d0fe19b8cbc4c9472dab5f3091581
Oliver Jehle <ojehle@yahoo.com>
2025-09-05 15:58:31 +0200

    update trixie

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t murks/docker-apache2-ssl-secure:latest .
+docker build -t murks/docker-apache2-ssl-secure:latest -t murks/docker-apache2-ssl-secure:trixie .
 docker push murks/docker-apache2-ssl-secure:latest
+docker push murks/docker-apache2-ssl-secure:trixie
